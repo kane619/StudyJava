bd3e9bb536f29104fa3749685fd18b972486be60
318026323@qq.com <061958lkf>
2020-11-02 15:49:55 +0800

    初始化项目

---
+++ src/com/company/netProgram/GreetingClient.java
@@ -0,0 +1,37 @@
+package com.company.netProgram;
+import java.net.*;
+import java.io.*;
+
+public class GreetingClient {
+    public static void main(String[] args) throws IOException  {
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        System.out.println("请输入主机名.");
+        System.out.println("Enter 'end' to quit.");
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while (!str.equals("end"));
+        String serverName = str;
+//        int port = Integer.parseInt(str2);
+        int port = 8080;
+        try
+        {
+            System.out.println("连接到主机：" + serverName + " ，端口号：" + port);
+            Socket client = new Socket(serverName, port);
+            System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("服务器响应： " + in.readUTF());
+            client.close();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
